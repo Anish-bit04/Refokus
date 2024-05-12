05519e7a2fdef3aff53c30ffd51288bfecf87916
Anish <Anish@lamarr.tech>
2024-05-12 19:59:36 +0530

    added product component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { IoIosReturnRight } from "react-icons/io";
 
-const Button = () => {
+const Button = ({title = "Get Started"}) => {
   return (
     <div className='bg-zinc-100 text-black h-9 w-40 rounded-full px-3 py-2 flex items-center justify-center gap-3'>
-      <span className=' text-sm font-medium'>Start a project</span>
+      <span className=' text-sm font-medium'>{title}</span>
       <IoIosReturnRight />
     </div>
   )
---
+++ src/components/Products.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Product from './Product'
+
+const Products = () => {
+    let products = [
+        {
+            title:"Arqitel",
+            description:"With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",
+            live:true,
+            case:false
+        },
+        {
+            title:"Cula",
+            description:"We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",
+            live:true,
+            case:false
+        },
+        {
+            title:"TTR",
+            description:"We designed and developed a magical gaming experience made in Webflow to promote the translation service and their sponsorship of the 2022 Webflow Conference.",
+            live:true,
+            case:false
+        },
+        {
+            title:"Like Magic",
+            description:"We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",
+            live:true,
+            case:true
+        }
+    ]
+
+  return (
+    <div className='bg-zinc-900'>
+       {products.map((val,index) => <Product val={val}/>)}
+    </div>
+  )
+}
+
+export default Products
