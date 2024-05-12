6a3a5e7101c33c24f107c5d926f4e73c6c014c1a
Anish <Anish@lamarr.tech>
2024-05-13 03:22:45 +0530

    added key

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     ]
   return (
     <div className='max-w-screen-xl mx-auto text-zinc-600 bg-zinc-900 flex items-center justify-between py-5'>
-        <div className='flex gap-10 '>{items.map(item=> <p className='text-sm'>{item}</p>)}</div>
+        <div className='flex gap-10 '>{items.map((item,index)=> <p key={index} className='text-sm'>{item}</p>)}</div>
         <img src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/6637ba0d8481b4339b1cda4f_Frame%2048097733.svg" alt="Enterprise" />
         
     </div>
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Marquee = ({imgurl}) => {
   return (
     <div className='flex items-center gap-14 w-full whitespace-nowrap overflow-hidden'>
-        {imgurl.map(url => <img src={url} alt='logo' className='w-32 flex-shrink-0 mt-14'/>)}
-        {imgurl.map(url => <img src={url} alt='logo' className='w-32 flex-shrink-0 mt-14'/>)}
+        {imgurl.map((url,index) => <img key={index} src={url} alt='logo' className='w-32 flex-shrink-0 mt-14'/>)}
+        {imgurl.map((url,index) => <img key={index} src={url} alt='logo' className='w-32 flex-shrink-0 mt-14'/>)}
     </div>
   )
 }
